ebefd6db78da6d0149559315cec214b8c85cab1e
S4ck <jcaveror@gmail.com>
2021-04-27 13:32:27 -0500

    primer commit

---
+++ js/form.js
@@ -0,0 +1,41 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  let telefono = "+51928717437";
+
+  let cliente = document.querySelector("#cliente").value;
+  let fecha = document.querySelector("#fecha").value;
+  let hora = document.querySelector("#hora").value;
+  let empleado = document.querySelector("#empleado").value;
+  let servicio = document.querySelector("#servicio").value;
+  let resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  let url = `https://api.whatsapp.com/send?phone=${+51928717437}&text=
+		*_Electromotriz Chavero_*%0A
+		*Reservas*%0A%0A
+		*¿Cuál es tu nombre?*%0A
+		${cliente}%0A
+		*Indica la fecha de tu servicio*%0A
+		${fecha}%0A
+		*Indica la hora de tu servicio*%0A
+		${hora}%0A
+		*Tipo de Envio*%0A
+		${empleado}%0A
+		*¿Cuál es el servicio que desea realizar?*%0A
+		${servicio}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu solicitud, ${cliente}`;
+
+  window.open(url);
+});
